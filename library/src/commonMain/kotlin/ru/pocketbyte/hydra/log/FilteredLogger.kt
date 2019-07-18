8ce1b7b9eb7df3f9955a9566864c78d35d40d1dc
Denis Shurygin <sdi.linch@gmail.com>
2019-07-18 23:13:14 +0300

    Add Logger extension function `filtered`

--- library/src/commonMain/kotlin/ru/pocketbyte/hydra/log/FilteredLogger.kt
+++ library/src/commonMain/kotlin/ru/pocketbyte/hydra/log/FilteredLogger.kt
@@ -51,3 +51,28 @@ open class FilteredLogger(
         }
     }
 }
+
+/**
+ * Returns Logger wrapped with FilteredLogger
+ *
+ * @property filter Filter function that defines which logs should be filtered
+ */
+fun Logger.filtered(filter: (level: LogLevel, tag: String?) -> Boolean): Logger {
+    return FilteredLogger(this, filter)
+}
+
+
+/**
+ * Returns Logger wrapped with FilteredLogger
+ *
+ * @param level Minimum log level that can be passed.
+ * Null if filter by LogLevel shouldn't be used.
+ * @param tags Set of tags that can be passed.
+ * Null if filter by Tag shouldn't be used.
+ */
+fun Logger.filtered(level: LogLevel? = null, tags: Set<String?>? = null): Logger {
+    if ((level == null || level == LogLevel.DEBUG) && tags.isNullOrEmpty())
+        return this
+
+    return FilteredLogger(this, level, tags)
+}
