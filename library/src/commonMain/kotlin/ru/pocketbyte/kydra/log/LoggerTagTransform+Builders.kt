5a83bed0d57af9a9c35fb21832e9369d91b4b65a
Denis Shurygin <sdi.linch@gmail.com>
2026-05-18 11:49:30 +0300

    Add tag transform with tag prefix and postfix

--- library/src/commonMain/kotlin/ru/pocketbyte/kydra/log/LoggerTagTransform+Builders.kt
+++ library/src/commonMain/kotlin/ru/pocketbyte/kydra/log/LoggerTagTransform+Builders.kt
@@ -18,6 +18,41 @@ fun <T : Logger> T.withTagTransform(
  *
  * @param defaultTag Tag that should be used if provided tag is null.
  */
+@Deprecated(
+    "Use withTag(prefix, default, postfix) instead.",
+    ReplaceWith("withTag(default = defaultTag)")
+)
 fun <T : Logger> T.withTag(defaultTag: String): LoggerTagTransform<T> {
     return LoggerTagTransform(this) { it ?: defaultTag }
+}
+
+/**
+ * Wraps Logger with Logger that transforms tags by applying an optional prefix, default value,
+ * and/or postfix.
+ *
+ * If the tag is null and [default] is also null, the tag remains null. Otherwise, the resolved tag
+ * is assembled as `"$prefix$tag$postfix"`, omitting any component that is null.
+ *
+ * @param prefix String prepended to the tag. Null means no prefix.
+ * @param default Fallback tag used when the provided tag is null. Null means tags are not replaced.
+ * @param postfix String appended to the tag. Null means no postfix.
+ */
+fun <T : Logger> T.withTag(
+    prefix: String? = null,
+    default: String? = null,
+    postfix: String? = null
+): LoggerTagTransform<T> {
+    return LoggerTagTransform(this) { tag ->
+        val tag = tag ?: default ?: return@LoggerTagTransform null
+
+        if (prefix != null || postfix != null) {
+            StringBuilder().apply {
+                prefix?.let { append(it) }
+                append(tag)
+                postfix?.let { append(it) }
+            }.toString()
+        } else {
+            tag
+        }
+    }
 }
--- library/src/commonTest/kotlin/ru/pocketbyte/kydra/log/LoggerTagTransformTest.kt
+++ library/src/commonTest/kotlin/ru/pocketbyte/kydra/log/LoggerTagTransformTest.kt
@@ -2,7 +2,6 @@ package ru.pocketbyte.kydra.log
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertSame
 
 class LoggerTagTransformTest {
 
@@ -27,7 +26,7 @@ class LoggerTagTransformTest {
     @Test
     fun testWithTagTagNotChanged() {
         val logger = TestLogger()
-        val loggerTransform = logger.withTag("default")
+        val loggerTransform = logger.withTag(default = "default")
 
         val level = LogLevel.WARNING
         val tag = "TEST_1"
@@ -43,7 +42,7 @@ class LoggerTagTransformTest {
     @Test
     fun testWithTagTagChanged() {
         val logger = TestLogger()
-        val loggerTransform = logger.withTag("default")
+        val loggerTransform = logger.withTag(default = "default")
 
         val level = LogLevel.WARNING
         val tag = null
@@ -54,5 +53,75 @@ class LoggerTagTransformTest {
         assertEquals(level, logger.level)
         assertEquals("default", logger.tag)
         assertEquals(message, logger.message)
+    }
+
+    @Test
+    fun testWithTagPrefixApplied() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(prefix = "pre-")
+
+        loggerTransform.log(LogLevel.DEBUG, "TAG") { "" }
+
+        assertEquals("pre-TAG", logger.tag)
+    }
+
+    @Test
+    fun testWithTagPostfixApplied() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(postfix = "-post")
+
+        loggerTransform.log(LogLevel.DEBUG, "TAG") { "" }
+
+        assertEquals("TAG-post", logger.tag)
+    }
+
+    @Test
+    fun testWithTagPrefixAndPostfixApplied() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(prefix = "pre-", postfix = "-post")
+
+        loggerTransform.log(LogLevel.DEBUG, "TAG") { "" }
+
+        assertEquals("pre-TAG-post", logger.tag)
+    }
+
+    @Test
+    fun testWithTagNullTagWithPrefixRemainsNull() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(prefix = "pre-")
+
+        loggerTransform.log(LogLevel.DEBUG, null) { "" }
+
+        assertEquals(null, logger.tag)
+    }
+
+    @Test
+    fun testWithTagNullTagWithDefaultAndPrefix() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(prefix = "pre-", default = "default")
+
+        loggerTransform.log(LogLevel.DEBUG, null) { "" }
+
+        assertEquals("pre-default", logger.tag)
+    }
+
+    @Test
+    fun testWithTagNullTagWithDefaultAndPostfix() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(default = "default", postfix = "-post")
+
+        loggerTransform.log(LogLevel.DEBUG, null) { "" }
+
+        assertEquals("default-post", logger.tag)
+    }
+
+    @Test
+    fun testWithTagNullTagWithDefaultPrefixAndPostfix() {
+        val logger = TestLogger()
+        val loggerTransform = logger.withTag(prefix = "pre-", default = "default", postfix = "-post")
+
+        loggerTransform.log(LogLevel.DEBUG, null) { "" }
+
+        assertEquals("pre-default-post", logger.tag)
     }
 }
